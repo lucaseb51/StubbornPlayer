19a3b52b6d49d12f751692fec0740d7a94050802
Lucas G Eastburn <leastburn@mail.csuchico.edu>
2018-03-01 23:05:06 -0800

    Applied functions

--- main.cpp
+++ main.cpp
@@ -5,7 +5,30 @@ using namespace std;
 
 int main()
 {
-  SecretDoor game;
-    
+int times;
+int wins=0;
+int i;
+SecretDoor Game;
+
+cout<<"How many times would you like to play the secret door game?"<<endl;
+cin>>times;
+
+while(i<times)
+    {
+
+   Game.newGame();
+   Game.guessDoorC();
+   Game.guessDoorC();
+   if(Game.isWinner()==true)
+        {
+            wins=wins+1;
+        }
+   times=times+1;
+
+
+    }
+
+cout<<"You have won a total of "<<wins<<" games out of "<<times<<endl;
+
   return 0;
 }
